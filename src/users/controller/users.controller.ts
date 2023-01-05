59988e91b344604f2b900ae90c596ffae9ed24e1
Xtian <xtiancuenta1@gmail.com>
2023-01-05 15:25:14 -0300

    Realizado paso 1

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,5 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { UsersService } from '../service/users.service';
+
+export class Usuario {
+  uuid: string;
+  nombre: string;
+  apellido: string;
+  email: string;
+}
+
+let usuarios: Usuario[]= [
+  {
+    uuid: '0',
+    nombre: 'Pepe',
+    apellido: 'Gomez',
+    email: 'pepe@gmail.com'
+  },
+  {
+    uuid: '1',
+    nombre: 'Martín',
+    apellido: 'Gomez',
+    email: 'martin@gmail.com'
+  },
+  {
+    uuid: '2',
+    nombre: 'Chris',
+    apellido: 'Gomez',
+    email: 'chris@gmail.com'
+  }
+]
+
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +38,63 @@ export class UsersController {
   @Get('message')
   getHello(): string {
     return this.usersService.getHello();
+  }
+
+  @Get('user')
+  getUser(): Usuario[] {
+    return usuarios;
+  }
+
+  @Get('user/:uuid')
+  getUserByUuid(@Param('uuid') id: string): Usuario {
+    let index: number = usuarios.findIndex(u => u.uuid == id);
+
+    return usuarios[index];
+  }
+
+  @Post('user')
+  postUser(@Body() body:Usuario): Usuario {
+    body.uuid = randomUUID();
+    usuarios.push(body);
+    return body;
+  }
+
+  @Put('user/:uuid')
+  putUser(@Param('uuid') id: string, @Body() body: Usuario): Usuario {
+    let index: number = usuarios.findIndex(u => u.uuid == id);
+    
+    usuarios[index].nombre = body.nombre;
+    usuarios[index].apellido = body.apellido;
+    usuarios[index].email = body.email;
+
+    return usuarios[index];
+  }
+
+  @Patch('user/:uuid')
+  patchUser(@Param('uuid') id: string, @Body() body: Usuario): Usuario {
+    let index: number = usuarios.findIndex(u => u.uuid == id);
+
+    if(body.nombre != null) {
+      usuarios[index].nombre = body.nombre;
+    }
+    if(body.apellido != null) {
+      usuarios[index].apellido = body.apellido;
+    }
+    if(body.email != null){
+      usuarios[index].email = body.email;
+    }
+
+    return usuarios[index];
+  }
+
+  @Delete('user/:id')
+  deleteUser(@Param('id') id: string): boolean {
+    let index: number = usuarios.findIndex(u => u.uuid == id);
+
+    if(index != -1) {
+      usuarios.splice(index, 1);
+      return true;
+    }
+    return false;
   }
 }
