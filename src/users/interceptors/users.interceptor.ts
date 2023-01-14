f6b0896648b7e4af2c14403d6ad11fae18f2aabd
Xtian <xtiancuenta1@gmail.com>
2023-01-14 10:16:04 -0300

    Se mejoró el interceptor usando instanceof Array

--- src/users/interceptors/users.interceptor.ts
+++ src/users/interceptors/users.interceptor.ts
@@ -1,26 +1,12 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor, Body } from '@nestjs/common';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
-
-// @Injectable()
-// export class UsersInterceptor implements NestInterceptor {
-//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//     return next.handle().pipe(map(users => {
-//       for (const user of users) {
-//         if(!user.lastName) {
-//           user.lastName = null;
-//         }
-//       }
-//       return users;
-//     }));
-//   }
-// }
 
 @Injectable()
 export class UsersInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(map(u => {
-      //Si u no tiene name, lo que llegó es una lista de usuarios
-      if(!u.name) {
+      //Si es una isntancia de un array, lo que llegó es una lista de usuarios
+      if(u instanceof Array) {
         for (const user of u) {
           if(!user.lastName) {
               user.lastName = null;
@@ -28,7 +14,7 @@ export class UsersInterceptor implements NestInterceptor {
           }
         return u;
       }
-      //Si u sí tiene name, es un usuario
+      //Si no, es un usuario
       if(!u.lastName) {
         u.lastName = null;
       }
